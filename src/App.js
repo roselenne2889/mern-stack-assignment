6331cdab246b8726cc29ddf4ba0f2294e855d1a7
beatriz2889 <beatriz.morales@georgebrown.ca>
2020-04-01 17:13:44 -0400

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AdminScreen from "./admin-screen/admin-screen";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/admin-screen">
+          <AdminScreen></AdminScreen>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/admin-screen/admin-screen.js
@@ -0,0 +1,8 @@
+import React from "react";
+class AdminScreen extends React.Component {
+  state = {};
+  render() {
+    return <button>Logout</button>;
+  }
+}
+export default AdminScreen;
